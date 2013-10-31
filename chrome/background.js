e0d0c2ceaee6afba6dd63f62d3f8f064981118f5
jimkang <jimkang@gmail.com>
2013-10-31 00:06:27 -0400

    Deleted some commented code.

--- chrome/background.js
+++ chrome/background.js
@@ -1,10 +1,6 @@
-// chrome.browserAction.setBadgeBackgroundColor({color:[240, 16, 8, 255]});
-// chrome.browserAction.setBadgeText({text: '200'});
-
-
 var reporter = createReporter();
 reporter.init();
 
-chrome.browserAction.onClicked.addListener(function respondToClick(tab) {
-});
+// chrome.browserAction.onClicked.addListener(function respondToClick(tab) {
+// });
 
