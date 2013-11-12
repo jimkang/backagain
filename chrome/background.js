27941933de5dbbfb12a09dd81858de990bf9ee10
jimkang <jimkang@gmail.com>
2013-11-11 22:31:45 -0500

    Cleanup.

--- chrome/background.js
+++ chrome/background.js
@@ -1,18 +1,2 @@
 var reporter = createReporter();
 reporter.init();
-
-// var scriptchain = createScriptChain(
-//   [
-//     'd3.v3.min.js', 
-//     'historyfilter.js', 
-//     'graph.js', 
-//     'drape.js', 
-//     'inpagereporter.js'
-//   ], 
-//   null, false
-// );
-
-// chrome.browserAction.onClicked.addListener(function respondToClick(tab) {
-  // scriptchain.loadChain();
-// });
-
